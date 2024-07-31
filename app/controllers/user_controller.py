9183414c7aef97a7893062eafc608875eadfee53
Auggustto <leonardoaugusto1000a@gmail.com>
2024-07-31 19:15:48 -0300

    The user creation schema was implemented in the models.

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,4 @@
+from app.models.user_model import UserModels
+
+
+class UserController(UserModels):
---
+++ app/models/user_model.py
@@ -0,0 +1,32 @@
+from app.models.database.models import User
+from app.models.database.session_db import get_db_session
+
+
+class UserModels:
+    
+    def check_user(self, email):
+        pass
+    
+    def create_user(self, metadata):
+        with get_db_session() as db:
+            
+            user = User(
+                name=metadata.name,
+                email=metadata.email,
+                password=metadata.password,
+                account_status=True
+                )
+            db.add(user)
+            db.commit()
+        
+        
+    def read_user(self, user_email):
+        pass
+    
+    def update_user(self, user_id, new_password):
+        pass
+    
+    def delete_user(self, user_id):
+        pass
+
+
