2c4a2202fc13d33a6db0c8a05ee4ae77e82b22b9
Auggustto <leonardoaugusto1000a@gmail.com>
2024-08-06 12:14:59 -0300

    The task table was added.

--- app/models/database/models.py
+++ app/models/database/models.py
@@ -11,10 +11,40 @@ class User(Base):
     password = Column(String, nullable=False)
     account_status = Column(Boolean, nullable=False)
     
+    tasks = relationship("Tasks", back_populates="user")
+    
     def as_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "email": self.email,
             "account_status": self.account_status
+        }
+        
+
+class Tasks(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    tags = Column(String(15), unique=False)
+    title = Column(String(50), unique=False)
+    description = Column(String, unique=False)
+    user_id = Column(Integer, unique=True)
+    status = Column(String, unique=False)
+    created_at = Column(DateTime, nullable=False)
+    due_date = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+    
+    user = relationship("User", back_populates="tasks")
+    
+    def as_dict(self):
+        return {
+            "tags": self.tags,
+            "title": self.title,
+            "description": self.description,
+            "user_id": self.user_id,
+            "status": self.status,
+            "created_at": self.created_at,
+            "due_date": self.due_date,
+            "updated_at": self.updated_at
         }
