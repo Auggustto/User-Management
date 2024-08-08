5c3c6b6c9c41a6c4003f2abfee291a74ba0abec7
Auggustto <leonardoaugusto1000a@gmail.com>
2024-08-08 16:32:54 -0300

    create pages for recovering password and create account

---
+++ frontend/src/services/login.js
@@ -0,0 +1,43 @@
+
+// Fetch data from API
+const BASE_URL = 'http://localhost:8000/task-schedule/api/user/login';
+
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('inputEmail').value;
+    const password = document.getElementById('inputPassword').value;
+
+    try {
+        const response = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            console.log(data)
+
+            // // Salvar os dados de login e JWT em cache
+            // localStorage.setItem('user', JSON.stringify(data.user)); 
+            // localStorage.setItem('jwt', data.token);
+
+            // // Redirecionar ou realizar outra ação após o login
+            // window.location.href = '/pagina-protegida.html';
+        } else {
+            // Lidar com erro de login
+            alert('Falha no login. Verifique suas credenciais.');
+        }
+    } catch (error) {
+        console.error('Erro ao fazer login:', error);
+    }
+});
+
+
+
+
+
