11c6542726be4a8209c7dc7ddf41c2f962d5e856
Auggustto <leonardoaugusto1000a@gmail.com>
2024-08-07 22:51:32 -0300

    The login, registration, and password recovery screens were implemented. The configuration of CORS in the backend has been started.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 ### import schemas of routers ###
 from app.routers.start import routers
@@ -8,6 +9,21 @@ from app.routers.tasks_router import tasks_routers
 
 
 app = FastAPI()
+
+# Lista de origens permitidas
+origins = [
+    "http://localhost:3000",  # Por exemplo, se o frontend estiver rodando nessa porta
+    "http://localhost:8000",  # Se você estiver consumindo a API de uma outra aplicação rodando em localhost
+]
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
 ### include all routers ###
 app.include_router(router=routers)
